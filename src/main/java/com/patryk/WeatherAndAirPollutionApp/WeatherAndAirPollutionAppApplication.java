1a7c513d33baf9b923c8a4d28b1ac7f4665eccb2
Patryk Fidrych <perevistp@gmail.com>
2021-03-09 18:18:54 +0100

    Initial commit

---
+++ src/main/java/com/patryk/WeatherAndAirPollutionApp/WeatherAndAirPollutionAppApplication.java
@@ -0,0 +1,12 @@
+package com.patryk.WeatherAndAirPollutionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAndAirPollutionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAndAirPollutionAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/patryk/WeatherAndAirPollutionApp/WeatherAndAirPollutionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patryk.WeatherAndAirPollutionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAndAirPollutionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
